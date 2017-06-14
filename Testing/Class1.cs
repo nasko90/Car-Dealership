2d7ba1bad1e05cc70561697899dcf7b397b43194
nasko90 <nasko_drenovichki@abv.bg>
2017-06-14 22:14:47 +0300

    creating db with entity code-first

---
+++ Testing/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealershi.Context.Context;
+using CarDealership.Models;
+
+namespace Testing
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            var context = new CarDealershipContext();
+            var model = new CarModel { Name = "Civic", Manufacturer = "Honda" };
+            var car = new Car
+            {
+                VIN = "12345678901234567111",
+                Price = 15000,
+                YearProduced = 2005
+            };
+            var saleInfo = new SaleInfo { Car = car, SalesDate = new DateTime(2016, 06, 06) };
+
+            car.SalesInfo = saleInfo;
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealershi.Context.Context;
+using CarDealership.Models;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CarDealershipContext();
+            var model = new CarModel { Name = "Civic", Manufacturer = "Honda" };
+            var car = new Car
+            {
+                VIN = "12345678901234567",
+                Price = 15000,
+                YearProduced = 2005,
+                CarModel = model
+            };
+            var saleInfo = new SaleInfo { Car = car, SalesDate = new DateTime(2016, 06, 06) };
+
+            car.SalesInfo = saleInfo;
+
+        }
+    }
+}
